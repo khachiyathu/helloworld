f4608a7815ee244043857d05808fc193b115cbf1
321D24016 <140032768+321D24016@users.noreply.github.com>
2023-08-03 23:14:00 +0900

    add

--- member.py
+++ member.py
@@ -70,6 +70,9 @@ mlist.append(newmember)
 newmember = Member("小林虎太郎", "よろしくです")
 mlist.append(newmember)
 
+newmember = Member("小島 匠人", "よろしくです")
+mlist.append(newmember)
+
 # メンバー表示
 print("各メンバーから一言")
 print()
