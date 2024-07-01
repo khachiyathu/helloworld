b03ed474071060a44f43531ee448abb0b0eb9380
Sakuma0112 <haya_0112@icloud.com>
2024-07-01 13:57:52 +0900

    Update member.py

--- member.py
+++ member.py
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 ver = "0.3"
+=======
+ver = "0.2"
+>>>>>>> Stashed changes
 
 class Member:
     def __init__(self, name, words=""):
@@ -17,8 +21,13 @@ newmember = Member("江頭2:50", "エガちゃんです！")
 mlist.append(newmember)
 
 ### 以下に自分を追加する ###
+<<<<<<< Updated upstream
 
 newmember = Member("伊藤圭", "よろしく")
+=======
+newmember = Member("佐久間駿太", "3年です！")
+mlist.append(newmember)
+>>>>>>> Stashed changes
 
 newmember = Member("高野瞬", "こんばんは")
 
