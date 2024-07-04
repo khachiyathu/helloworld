0ece1750b0b1a181ad2ebae34c7ca572bce570cc
besan1943-del <s.shota3070@gmail.com>
2024-07-04 13:36:06 +0900

    add new mamber

--- member.py
+++ member.py
@@ -2,16 +2,13 @@
 
 
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-ver = "0.3"
-=======
-ver = "0.14"
-=======
-ver = "0.2"
->>>>>>> Stashed changes
 
->>>>>>> 54d61dc7068851e05923c46e033d27a675c3f41e
+ver = "0.3"
+
+ver = "0.14"
+
+ver = "0.2"
+
 class Member:
     def __init__(self, name, words=""):
         self.name = name
@@ -29,8 +26,7 @@ newmember = Member("江頭2:50", "エガちゃんです！")
 mlist.append(newmember)
 
 ### 以下に自分を追加する ###
-<<<<<<< Updated upstream
-<<<<<<< HEAD
+
 newmember = Member("三瓶祥太", "よろしく！")
 mlist.append(newmember)
 
@@ -40,7 +36,6 @@ newmember = Member("蜂屋孝太郎", "アタオカです！")
 
 newmember = Member("服部一花", "姓の読みはハットリです")
 
-=======
 newmember = Member("田部大地", "あたおかです！")
 mlist.append(newmember)
 newmember = Member("和田龍飛", "こんにちは")
@@ -51,15 +46,14 @@ mlist.append(newmember)
 newmember = Member("齋藤空", "よろしくお願いします！")
 mlist.append(newmember)
 newmember = Member("蜂屋孝太郎", "アタオカです！")
->>>>>>> 54d61dc7068851e05923c46e033d27a675c3f41e
 mlist.append(newmember)
 newmember = Member("伊藤圭", "よろしく")
-<<<<<<< HEAD
+
 
 newmember = Member("石垣瑠平", "お願いします")
 
 
-=======
+
 mlist.append(newmember)
 newmember = Member("佐久間駿太", "3年です！")
 mlist.append(newmember)
@@ -67,25 +61,19 @@ newmember = Member("服部一花", "姓の読みはハットリです")
 mlist.append(newmember)
 newmember = Member("石垣瑠平", "お願いします")
 mlist.append(newmember)
->>>>>>> 54d61dc7068851e05923c46e033d27a675c3f41e
 newmember = Member("高野瞬", "こんばんは")
 mlist.append(newmember)
 newmember = Member("伊美祐希", "よろしくお願いいたします。")
-<<<<<<< HEAD
-=======
 newmember = Member("三瓶祥太", "あたおかです！")
 mlist.append(newmember)
->>>>>>> Stashed changes
 
-
-=======
 mlist.append(newmember)
 newmember = Member("山口莉歩", "ヤマグチです！")
 mlist.append(newmember)
 newmember = Member("綱島隼斗", "おはようございます")
 mlist.append(newmember)
              
->>>>>>> 54d61dc7068851e05923c46e033d27a675c3f41e
+
 # メンバー表示
 print("各メンバーから一言")
 print()
